73a181beb774327cdfb8544d4ee5176751a29bba
Harley Salas <harley@harleysalas.com>
2024-07-27 12:42:16 +0300

    fix: RichText imports

--- src/components/blocks/FormBlock/Fields/Message.tsx
+++ src/components/blocks/FormBlock/Fields/Message.tsx
@@ -1,11 +1,11 @@
 import type { MessageField } from '@payloadcms/plugin-form-builder/types'
 
-import RichText from '@/components/RichText'
+import { RichText } from '@/components/RichText'
 import React from 'react'
 
 import { Width } from './Width'
 
-export const Message: React.FC<MessageField> = ({ message }) => {
+export const Message: React.FC<MessageField> = ({ message }: { message: any }) => {
   return (
     <Width className="my-12" width="100">
       <RichText content={message} />
